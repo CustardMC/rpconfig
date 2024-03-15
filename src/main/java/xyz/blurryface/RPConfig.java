416b872fdb73da10339cce29ee92955e255f0bb7
blryface <124838505+blryface@users.noreply.github.com>
2024-03-15 14:29:37 -0300

    :snail: real

---
+++ src/main/java/xyz/blurryface/RPConfig.java
@@ -0,0 +1,13 @@
+package xyz.blurryface;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RPConfig implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("rpconfig");
+	@Override
+	public void onInitialize() {
+	}
+}
